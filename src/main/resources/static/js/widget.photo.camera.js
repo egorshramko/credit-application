8b097ba1bbc5148e2e9bb2e5ae1ad7ac4e41e0c6
Egor Shramko <egorii2001@gmail.com>
2025-04-11 03:51:13 +0700

    Created load profile photo by web camera

---
+++ src/main/resources/static/js/widget.photo.camera.js
@@ -0,0 +1,134 @@
+$("#camera-photo-btn").on('click', camera_photo_btn_handler);
+$("#camera-popup").on('hide.bs.modal', function () {
+    $(document.activeElement).blur();
+});
+
+$("#clear-photo-btn").on('click', clear_photo);
+$("#photo-load-widget-container").on('mouseover', show_widget_controls);
+$("#photo-load-widget-container").on('mouseout', hide_widget_controls);
+
+function camera_photo_btn_handler(event) {
+
+    //вычисляем ширину и высоту камеры
+    let camera_area = $("#camera-area");
+    let width = camera_area.width; //ширина фотографии
+    let height = 0; //вычислится на основе входящего потока
+
+    //видеопоток активен
+    let streaming = false;
+
+    //ссылки на соответствующие элементы виджета
+    let video_container = document.getElementById("camera-input-container");
+    let video = document.getElementById("camera-input");
+    let canvas = document.getElementById("camera-canvas");
+    let photo = document.getElementById("loaded-photo");
+    let camera_photo_btn = document.getElementById("take-photo-btn");
+    let no_camera_img = document.getElementById("no-camera-img");
+
+    //получаем медиапоток
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(err) {
+            console.error("Произошла ошибка: " + err);
+        });
+
+    video.addEventListener('canplay', function(event) {
+        if (!streaming) {
+
+            video_container.classList.remove('d-none');
+            no_camera_img.classList.add('d-none');
+
+            console.log("Запускаем работу вебки");
+
+            height = video.videoHeight / (video.videoWidgth / width);
+
+            //canvas.setAttribute('width', video.offsetWidth);
+            //canvas.setAttribute('heigth', video.offsetHeight);
+            
+            streaming = true;
+        }
+    }, false);
+
+    let take_photo_btn = $("#take-photo-btn");
+    take_photo_btn.on('click', function (event) {
+        take_picture();
+        event.preventDefault();
+    });
+}
+
+function take_picture() {
+    let canvas = document.getElementById("camera-canvas");
+    let video = document.getElementById('camera-input');
+
+    canvas.setAttribute('width', video.offsetWidth);
+    canvas.setAttribute('height', video.offsetHeight);
+    let width = canvas.width;
+    let height = canvas.height;
+
+    let canvas_context = canvas.getContext('2d');
+    let no_camera_img = document.getElementById('no-camera-img');
+
+    //если не светится заглушка веб-камеры, когда до нее нет доступа
+    if (no_camera_img.classList.contains('d-none')) {
+        canvas_context.drawImage(video, 0, 0, width, height);
+
+        let photoUrl = canvas.toDataURL('/static/assets/camera/avatar.png');
+        let loaded_photo = document.getElementById('loaded-photo');
+        loaded_photo.setAttribute('src', photoUrl);
+
+        let photo_load_widget = document.getElementById('photo-load-widget');
+        if (!photo_load_widget.classList.contains('photo-uploaded')) {
+            photo_load_widget.classList.add('photo-uploaded');
+        }
+        
+    }
+    else {
+        clear_photo();
+    }
+}
+
+function clear_photo() {
+    $("#clear-photo-btn").blur();
+    console.log("Сработала очистка фото");
+
+    let loaded_photo = document.getElementById('loaded-photo');
+    //loaded_photo.src = './static/assets/profile/empty_avatar.png';
+    loaded_photo.setAttribute('src', '/static/assets/profile/empty_avatar.png');
+
+    let photo_load_widget = document.getElementById('photo-load-widget');
+    if (photo_load_widget.classList.contains('photo-uploaded')) {
+        photo_load_widget.classList.remove('photo-uploaded');
+    }
+    
+}
+
+function show_widget_controls() {
+    let photo_widget_container = document.getElementById('photo-load-widget-container');
+    let controls = photo_widget_container.querySelectorAll('.photo-widget-control');
+    controls.forEach((control) => {
+        if (control.classList.contains('hide-photo-widget-control')) {
+
+            if (control.id != 'clear-photo-btn' || 
+                control.id == 'clear-photo-btn' && 
+                $("#photo-load-widget").hasClass('photo-uploaded')) {
+                    
+                control.classList.remove('hide-photo-widget-control');
+            }
+            
+        }
+    });
+}
+
+function hide_widget_controls() {
+    let photo_widget_container = document.getElementById('photo-load-widget-container');
+    let controls = photo_widget_container.querySelectorAll('.photo-widget-control');
+    controls.forEach((control) => {
+        if (!control.classList.contains('hide-photo-widget-control')) {
+            control.classList.add('hide-photo-widget-control');
+        }
+    });
+}
+
