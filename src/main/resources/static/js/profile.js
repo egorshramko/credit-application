3b092de30da1795a4305b1994033f4e3fa9a69f9
Egor Shramko <egorii2001@gmail.com>
2025-05-02 18:11:40 +0700

    Created profile form validation before sending form

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,6 +1,22 @@
-$("#passport-series-input").click().mask("99 99");
-$("#passport-number-input").click().mask("999999");
-$("#passport-department-code-input").click().mask("999-999");
+$("#passport-series-input").click().mask("99 99", {
+	completed: function() {
+		passport_series_input_handler();
+	},
+	autoclear: false
+});
+
+$("#passport-number-input").click().mask("999999", {
+	completed: function() {
+		passport_number_input_handler();
+	},
+	autoclear: false
+});
+$("#passport-department-code-input").click().mask("999-999", {
+	completed: function() {
+		passport_department_code_input_handler();
+	},
+	autoclear: false
+});
 $("#tin-input").click().mask("999999999999");
 
 
@@ -10,14 +26,23 @@ $("#middlename-input").on('input', middlename_input_handler);
 
 $("#birthdate-input").on('change', birthdate_change_handler);
 
+$("#citizenship-input").on('input', citizenship_input_handler);
+$("#sex-input").on('change', sex_change_handler);
+
+$("#passport-issue-date-input").on('change', passport_issue_date_input_handler);
+$("#passport-issue-place-input").on('input', passport_issue_place_input_handler);
+
+$("#consent-personal-data-input").on('change', consent_personal_data_input_handler);
+
 $("#return-index-link").on('click', return_index_link_handler);
+$("#send-profile-btn").on('click', sendProfileHandler);
 
 //обработчик ввода фамилии
 function lastname_input_handler() {
 
     let lastname_field = $("#lastname-input");
     let input_valid = check_input(lastname_field);
-    highlight_invalid_field(lastname_field, input_valid);
+    highlight_invalid_field(lastname_field, input_valid, "Допустимые символы: кириллица, -, пробел");
 
 }
 
@@ -26,7 +51,7 @@ function firstname_input_handler() {
 
     let firstname_field = $("#firstname-input");
     let input_valid = check_input(firstname_field);
-    highlight_invalid_field(firstname_field, input_valid);
+    highlight_invalid_field(firstname_field, input_valid, "Допустимые символы: кириллица, -, пробел");
 
 }
 
@@ -35,7 +60,64 @@ function middlename_input_handler() {
 
     let middlename_field = $("#middlename-input");
     let input_valid = check_input(middlename_field);
-    highlight_invalid_field(middlename_field, input_valid);
+    highlight_invalid_field(middlename_field, input_valid, "Допустимые символы: кириллица, -, пробел");
+}
+
+function citizenship_input_handler() {
+	let citizenship_field = $("#citizenship-input");
+	highlight_invalid_field(citizenship_field);
+}
+
+function sex_change_handler() {
+	let sex_field = $("#sex-input");
+	highlight_invalid_field(sex_field);
+}
+
+function passport_series_input_handler() {
+	
+	let passport_series = $("#passport-series-input");
+	highlight_invalid_field(passport_series);
+}
+
+function passport_number_input_handler() {
+	let passport_number = $("#passport-number-input");
+	highlight_invalid_field(passport_number);
+}
+
+function passport_issue_date_input_handler() {
+	
+	let dateControl = $('#passport-issue-date-input');
+		
+		if (!!dateControl.val()) {
+			let inputDate = new Date(dateControl.val());
+			inputDate.setHours(0, 0, 0, 0);
+				
+			let todayDate = new Date();
+			todayDate.setHours(0, 0, 0, 0);
+				
+			highlight_invalid_field(dateControl, inputDate <= todayDate, "Пожалуйста, введите корректную дату");
+			
+		}
+		else {
+			highlight_invalid_field(dateControl);
+		}
+		
+}
+
+function passport_department_code_input_handler() {
+	let passport_department_code = $("#passport-department-code-input");
+	console.log(passport_department_code.val());
+	highlight_invalid_field(passport_department_code);
+}
+
+function passport_issue_place_input_handler() {
+	let passport_issue_place = $("#passport-issue-place-input");
+	highlight_invalid_field(passport_issue_place);
+}
+
+function consent_personal_data_input_handler() {
+	let consent_personal_data_input = $("#consent-personal-data-input");
+	highlight_invalid_field(consent_personal_data_input);
 }
 
 //Проверка корректности ввода
@@ -47,8 +129,15 @@ function check_input(element) {
 }
 
 //Подсветка некорректно заполненного поля
-function highlight_invalid_field(element, input_valid) {
-    if (!input_valid) {
+function highlight_invalid_field(element, input_valid = true, message = "") {
+    
+	//родительский контейнер
+	let parent_div = element.parent();
+	let invalid_feedback_div = parent_div.find(".invalid-feedback");
+	let invalid_feedback_span = invalid_feedback_div.find("span");
+	invalid_feedback_span.text(message);
+	
+	if (!input_valid) {
         if (!element.hasClass('is-invalid')) {
             element.addClass('is-invalid');
         }
@@ -65,16 +154,28 @@ function birthdate_change_handler(event) {
 	
 	let dateControl = $('#birthdate-input');
 	
-	let inputDate = new Date(dateControl.val());
-	inputDate.setHours(0, 0, 0, 0);
+	if (!!dateControl.val()) {
+		let inputDate = new Date(dateControl.val());
+		inputDate.setHours(0, 0, 0, 0);
+			
+		let todayDate = new Date();
+		todayDate.setHours(0, 0, 0, 0);
+			
+		let maxInputDate = new Date(todayDate);
+		maxInputDate.setFullYear(maxInputDate.getFullYear() - 18);
+			
+		highlight_invalid_field(dateControl, inputDate < maxInputDate, "Клиент должен быть старше 18 лет");
+		
+		if (inputDate > todayDate) {
+			highlight_invalid_field(dateControl, false, "Пожалуйста, введите корректную дату");
+		}
+	}
+	else {
+		highlight_invalid_field(dateControl);
+	}
 	
-	let todayDate = new Date();
-	todayDate.setHours(0, 0, 0, 0);
 	
-	let maxInputDate = new Date(todayDate);
-	maxInputDate.setFullYear(maxInputDate.getFullYear() - 18);
 	
-	highlight_invalid_field(dateControl, inputDate < maxInputDate);
 	
 }
 
@@ -148,4 +249,108 @@ async function removeFileByUuid(uuid) {
 	});
 	
 	return response;
+}
+
+//обработчик нажатия на кнопку "Заполнить анкету"
+function sendProfileHandler(event) {
+	
+	console.log("Валидация обязательных полей");
+	
+	let validateResult = validateProfileForm();
+	
+}
+
+function validateField(element, baseMessage = "", additionalChecks = true) {
+	let elementValue = element.val();
+	let elementInvalid = elementValue == "" || elementValue == null ||
+		elementValue == undefined || element.hasClass('is-invalid') || !additionalChecks;
+		
+	if (elementInvalid) {
+		
+		let invalidMessage = baseMessage;
+		if (!element.hasClass('is-invalid') || invalidMessage == "") {
+			invalidMessage = "Пожалуйста, заполните обязательное поле";
+		}
+		
+		highlight_invalid_field(element, !elementInvalid, invalidMessage);
+		
+		return false;
+	}
+	
+	highlight_invalid_field(element, !elementInvalid, "");
+	
+	return true;
+}
+
+//функция проверки обязательных полей заполнения анкеты
+function validateProfileForm() {
+	let validated = true;
+	
+	//проверка фамилии
+	if (!validateField($("#lastname-input"), "Допустимые символы: кириллица, -, пробел")) {
+		validated = false;
+	}
+	
+	//проверка имени
+	if (!validateField($("#firstname-input"), "Допустимые символы: кириллица, -, пробел")) { 
+		validated = false;
+	}
+	
+	//проверка даты рождения
+	if (!validateField($("#birthdate-input"), "Клиент должен быть старше 18 лет")) {
+		validated = false;
+	}
+	
+	//проверка гражданства
+	if (!validateField($("#citizenship-input"))) {
+		validated = false;
+	}
+	
+	//проверка пола
+	if (!validateField($("#sex-input"))) {
+		validated = false;
+	}
+	
+	//проверка серии паспорта
+	if (!validateField($("#passport-series-input"), 
+		undefined, 
+		/^\d{2}\s{1}\d{2}$/g.test($("#passport-series-input").val()))) {
+		
+		validated = false;
+	}
+	
+	//проверка номера паспорта
+	if (!validateField($("#passport-number-input"),
+		undefined,
+		/^\d{6}$/g.test($("#passport-number-input").val()))) {
+		
+		validated = false;
+	}
+	
+	//проверка даты выдачи паспорта
+	if (!validateField($("#passport-issue-date-input"))) {
+		validated = false;
+	}
+	
+	//проверка кода подразделения
+	if (!validateField($("#passport-department-code-input"),
+		undefined,
+		/^\d{3}\-{1}\d{3}$/g.test($("#passport-department-code-input").val()))) {
+		validated = false;
+	}
+	
+	//проверка места выдачи паспорта
+	if (!validateField($("#passport-issue-place-input"))) {
+		validated = false;
+	}
+	
+	//проверка согласия на обработку персональных данных
+	let consentPersonalDataCheckbox = $("#consent-personal-data-input");
+	let givesConsentPersonalData = consentPersonalDataCheckbox.is(":checked");
+	if (!givesConsentPersonalData) {
+		highlight_invalid_field(consentPersonalDataCheckbox, givesConsentPersonalData, "Пожалуйста, дайте согласие на обработку данных");
+	}
+	
+	
+	return validated;
 }
