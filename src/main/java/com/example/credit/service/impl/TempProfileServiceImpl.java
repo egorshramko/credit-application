e1ade178b689d53ec192c624d757cbeb18fd4f1a
Egor Shramko <egorii2001@gmail.com>
2025-05-12 21:28:13 +0700

    Fix issue #1

--- src/main/java/com/example/credit/service/impl/TempProfileServiceImpl.java
+++ src/main/java/com/example/credit/service/impl/TempProfileServiceImpl.java
@@ -5,18 +5,23 @@ import com.example.credit.data.ClientProfile;
 import com.example.credit.data.Passport;
 import com.example.credit.data.PassportScan;
 import com.example.credit.service.TempProfileService;
+import com.example.credit.storage.service.TempStorageService;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class TempProfileServiceImpl implements TempProfileService {
 
+	@Autowired
+	private TempStorageService tempStorageService;
+	
 	@Override
 	public ClientProfile addPassportScan(ClientProfile profile, 
 			UUID scanUUID) {
@@ -24,6 +29,7 @@ public class TempProfileServiceImpl implements TempProfileService {
 		Passport passport = profile.getPassport();
 		passport.addScan(PassportScan.builder()
 				.scanFile(BinaryContent.builder()
+						.name(tempStorageService.getFileNameById(scanUUID))
 						.uuid(scanUUID)
 						.build())
 				.build());
