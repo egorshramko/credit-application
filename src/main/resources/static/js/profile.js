30fbac8fbfd5aab844b779a519ce39eee923de44
Egor Shramko <egorii2001@gmail.com>
2025-06-03 00:56:36 +0700

    Finished please wait dialog

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -291,6 +291,18 @@ async function sendProfileHandler(event) {
 		//раскомменчу, когда разберусь с модальным окошком
 		sendFormDataJson(formDataJson)
 			.then(() => {
+				
+				let pleaseWaitMessage = document.getElementById('please-wait-message');
+				pleaseWaitMessage.innerText = "Анкета обработана. Перенаправление на заполнение заявки...";
+				//$("#please-wait-message").text("Анкета обработана. Перенаправление на заполнение заявки...");
+				
+				setTimeout(() => {}, 2000);
+				
+				//так ли оно надо????
+				//$("#please-wait-message").text("Анкета обрабатывается. Пожалуйста, подождите...");
+				
+			})
+			.then(() => {
 				$("#please-wait-dialog").modal('hide');
 			})
 			.catch(() => {
