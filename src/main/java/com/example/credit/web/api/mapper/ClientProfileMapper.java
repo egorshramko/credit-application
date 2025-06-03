72c15d19729896b7f8c6f10a18a0ae3390a5a692
Egor Shramko <egorii2001@gmail.com>
2025-06-03 20:37:22 +0700

    Add update method to profile mapper

--- src/main/java/com/example/credit/web/api/mapper/ClientProfileMapper.java
+++ src/main/java/com/example/credit/web/api/mapper/ClientProfileMapper.java
@@ -1,18 +1,32 @@
 package com.example.credit.web.api.mapper;
 
 import com.example.credit.data.ClientProfile;
+import com.example.credit.data.Contact;
+import com.example.credit.data.enums.ContactType;
+import com.example.credit.data.enums.Sex;
 import com.example.credit.web.api.dto.profile.ClientProfileDto;
+import com.example.credit.web.api.dto.profile.ContactDto;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Slf4j
 @Component
 public class ClientProfileMapper {
+	
+	@Autowired
+	private PassportMapper passportMapper;
+	
+	@Autowired
+	private BinaryContentMapper binaryContentMapper;
 	
 	public ClientProfileDto toDTO(ClientProfile profile) {
 		
@@ -30,6 +44,54 @@ public class ClientProfileMapper {
 				.comment(profile.getComment())
 				.consentPersonalData(profile.getConsentPersonalData())
 				.build();
+	}
+	
+	public ClientProfile createProfileFromDto(ClientProfileDto profileDto) {
+		return ClientProfile.builder()
+				.build();
+	}
+	
+	public ClientProfile updateProfileFromDto(ClientProfile profile, ClientProfileDto profileDto) {
+		
+		profile.setLastname(profileDto.getLastname());
+		profile.setFirstname(profileDto.getFirstname());
+		profile.setMiddlename(profileDto.getMiddlename());
+		profile.setBirthdate(LocalDate.parse(profileDto.getBirthdate(), 
+											DateTimeFormatter.ISO_LOCAL_DATE));
+		profile.setTin(profileDto.getTin());
+		profile.setCitizenship(profileDto.getCitizenship());
+		profile.setSex(Sex.valueOf(profileDto.getSex()));
+		profile.setConsentPersonalData(profileDto.getConsentPersonalData());
+		profile.setComment(profileDto.getComment());
+		
+		profile.setPassport(
+				passportMapper.updatePassportFromDto(
+						profile.getPassport(), 
+						profileDto.getPassport()));
+		
+		//обновление существующих контактов
+		for (Contact contact : profile.getContacts()) {
+			ContactDto contactDto = profileDto.getContacts()
+					.stream()
+					.filter(cdto -> cdto.getUuid().equals(contact.getUuid().toString()))
+					.findFirst()
+					.orElse(null);
+			
+			if (contactDto != null) {
+				contact.setContactType(ContactType.valueOf(contactDto.getContactType()));
+				contact.setPhoneNumber(contactDto.getPhoneNumber());
+				contact.setComment(contactDto.getComment());
+			}
+			
+		}
+		
+		profile.setPhoto(
+				binaryContentMapper
+						.toBinaryContent(profileDto.getPhoto()));
+		
+		
+		return profile;
+		
 	}
 	
 }
