3043aa0ca4a090b8d2a97bfeff5e1b4b6cce636e
Egor Shramko <egorii2001@gmail.com>
2025-06-15 22:02:43 +0700

    Fixed initializing loan product dictionary

--- src/main/java/com/example/credit/CreditApplication.java
+++ src/main/java/com/example/credit/CreditApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.example.credit.data")
-@EnableJpaRepositories("com.example.credit.data.repository")
+@EntityScan({"com.example.credit.data", 
+	"com.example.credit.application.data"})
+@EnableJpaRepositories({"com.example.credit.data.repository", 
+	"com.example.credit.application.repository"})
 public class CreditApplication {
 
 	public static void main(String[] args) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,19 @@
 INSERT INTO loan_product (id, name, rate, min_term, max_term, min_amount, max_amount, currency)
 	VALUES 
-		(1, 'Микрозайм под минимальный процент', 1.25, 3, 6, 2000, 10000, 'RUB'),
-		(2, 'Кредит наличными на еду', 0.25, 12, 60, 50000, 10000000, 'RUB'),
-		(3, 'Долларовый кредит', 0.37, 12, 36, 1000, 5000, 'USD'),
+		(1, 'Микрозайм под минимальный процент', 1.25, 3, 6, 2000, 10000, 'RUB')
+		ON CONFLICT (id) DO NOTHING;
+		
+INSERT INTO loan_product (id, name, rate, min_term, max_term, min_amount, max_amount, currency)
+	VALUES
+		(2, 'Кредит наличными на еду', 0.25, 12, 60, 50000, 10000000, 'RUB')
+		ON CONFLICT (id) DO NOTHING;
+		
+INSERT INTO loan_product (id, name, rate, min_term, max_term, min_amount, max_amount, currency)
+	VALUES
+		(3, 'Долларовый кредит', 0.37, 12, 36, 1000, 5000, 'USD')
+		ON CONFLICT (id) DO NOTHING;
+		
+INSERT INTO loan_product (id, name, rate, min_term, max_term, min_amount, max_amount, currency)
+	VALUES
 		(4, 'Ипотека на всю жизнь', 0.45, 60, 120, 5000000, 40000000, 'RUB')
 	ON CONFLICT (id) DO NOTHING;
