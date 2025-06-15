f3f35a45d827fc06990c9f3e26122c4daa7ef92c
Egor Shramko <egorii2001@gmail.com>
2025-06-15 21:29:03 +0700

    Added validation for LoanProduct class

--- src/main/java/com/example/credit/application/data/LoanProduct.java
+++ src/main/java/com/example/credit/application/data/LoanProduct.java
@@ -2,8 +2,10 @@ package com.example.credit.application.data;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,16 +19,23 @@ public class LoanProduct {
 	@Id
 	private Long id;
 	
-	private final LocalDateTime createdDate = LocalDateTime.now();
-	
+	@Column(unique = true)
+	@NotNull
 	private String name;
 	
+	@NotNull
 	private Double rate;
 	
+	@NotNull
 	private Integer minTerm;
+	
+	@NotNull
 	private Integer maxTerm;
 	
+	@NotNull
 	private Integer minAmount;
+	
+	@NotNull
 	private Integer maxAmount;
 	
 }
