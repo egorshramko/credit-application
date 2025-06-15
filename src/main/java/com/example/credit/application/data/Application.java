e724560b2abf192d2aa6f70639568bba5d34663a
Egor Shramko <egorii2001@gmail.com>
2025-06-16 00:15:20 +0700

    Added builder annotation to application class

--- src/main/java/com/example/credit/application/data/Application.java
+++ src/main/java/com/example/credit/application/data/Application.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Application {
 	
 	@Id
