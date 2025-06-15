1e69333a2c1a5a4b9a0266a132cb7d2dc2def016
Egor Shramko <egorii2001@gmail.com>
2025-06-15 23:22:17 +0700

    Created getApplicationByCreditId method realization in application service

--- src/main/java/com/example/credit/application/service/impl/ApplicationServiceImpl.java
+++ src/main/java/com/example/credit/application/service/impl/ApplicationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.credit.application.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.credit.application.data.Application;
+import com.example.credit.application.repository.ApplicationRepository;
 import com.example.credit.application.service.ApplicationService;
+import com.example.credit.data.Credit;
+import com.example.credit.service.CreditService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,10 +15,30 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
 
+	@Autowired
+	private CreditService creditService;
+	
+	@Autowired
+	private ApplicationRepository appRepository;
+	
 	@Override
 	public Application getApplicationByCreditId(String creditId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		//получение объекта кредита
+		Credit credit = creditService.getCreditById(creditId);
+		
+		//если по кредиту уже существует заявка, то возвращаем эту заявку
+		if (credit.getApplication() != null) {
+			return credit.getApplication();
+		}
+		//иначе создаем заявку и возвращаем новую
+		else {
+			Application app = appRepository.save(new Application());
+			credit.setApplication(app);
+			creditService.saveCredit(credit);
+			return app;
+		}
+		
 	}
 
 	@Override
