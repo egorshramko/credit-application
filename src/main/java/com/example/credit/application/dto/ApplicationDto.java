55ef3f5214a51744929682eb559a012733e87910
Egor Shramko <egorii2001@gmail.com>
2025-06-24 21:24:44 +0700

    Added lombok annotations to ApplicationDto

--- src/main/java/com/example/credit/application/dto/ApplicationDto.java
+++ src/main/java/com/example/credit/application/dto/ApplicationDto.java
@@ -1,8 +1,12 @@
 package com.example.credit.application.dto;
 
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class ApplicationDto {
 	
 	private String currency;
